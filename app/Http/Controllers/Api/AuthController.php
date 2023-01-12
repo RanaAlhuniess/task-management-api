92964a51cafeb08698150d7494bacab6e5e1fc17
rana <rana.hanis93@gmail.com>
2023-01-12 17:07:44 +0200

    Added login Api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\User;
 use App\Http\Resources\Api\UserResource;
+
 class AuthController extends BaseController
 {
     public function register(Request $request): \Illuminate\Http\JsonResponse
@@ -27,10 +28,24 @@ class AuthController extends BaseController
                 'email' => $request->email,
                 'password' => $request->password
             ]);
-            return $this->respond( new UserResource($user));
+            return $this->respond(new UserResource($user));
         } catch (Exception $e) {
             $message = 'Oops! Unable to create a new user.';
             return $this->respondError($message, 500);
         }
+    }
+
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+        if (!auth()->attempt($data)) {
+            $message = ['error' => 'Unauthorised'];
+            return $this->respondError($message, 401);
+        }
+        $user = auth()->user();
+        return $this->respond(new UserResource($user));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::post('register', [AuthController::class, 'register']);
-//Route::post('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login']);

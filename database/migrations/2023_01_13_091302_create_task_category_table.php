b420d992865e7b4be48cb167323058de53c783d3
Rana Alhuniess <86882451+RanaAlhuniess@users.noreply.github.com>
2023-01-14 17:04:34 +0300

    Added model/migration for task's entites

---
+++ database/migrations/2023_01_13_091302_create_task_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_category', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('task_id');
+            $table->bigInteger('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_category');
+    }
+};
